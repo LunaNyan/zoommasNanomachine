5ed6f9a00129cca96ffc87641aeede32cd93c881
LunaNyan <chkong1998@naver.com>
2023-02-05 05:08:59 +0900

    속도 증가 (197400 → 402000)

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+import random
+
 import pyautogui
 import time
 import sys
+import ctypes
 from locations import *
 
 def mainWork(delay, detMin, detMax, offset):
     print("[Stage 1] 생고기 접시 이미지 매칭")
     print("색상 감지 최소값 : " + str(detMin))
     print("색상 감지 최대값 : " + str(detMax))
+    catchLimit = 30
+    cnt = 1
+    time.sleep(.2)
     for pos in areaWork:
         print(pos)
         catched = False
@@ -17,54 +23,71 @@ def mainWork(delay, detMin, detMax, offset):
                 for ax in range(areaTarget.X2 - areaTarget.X1):
                     px = im1.getpixel(xy=(ax, ay))
                     if detMin[0] <= px[0] <= detMax[0] and detMin[1] <= px[1] <= detMax[1] and detMin[2] <= px[2] <= detMax[2]:
-                        print("고기 발견")
-                        print("X_Pos=" + str(ax) + ", Y_Pos=" + str(ay))
-                        print("클릭 오프셋 : " + str(offset))
-                        pyautogui.moveTo(areaTarget.X1 + ax + offset[0], areaTarget.Y1 + ay + offset[1])
-                        pyautogui.mouseDown()
-                        pyautogui.moveTo(pos[0], pos[1])
-                        pyautogui.mouseUp()
+                        print("고기 발견 (" + str(ax) + ", " + str(ay) + ", 색상값 : " + str(px) + ")")
                         catched = True
+                        catchLimit -= 1
+                        print("클릭 오프셋 : " + str(offset))
+                        ctypes.windll.user32.SetCursorPos(areaTarget.X1 + ax + offset[0], areaTarget.Y1 + ay + offset[1])
+                        ctypes.windll.user32.mouse_event(2, 0, 0, 0, 0)
+                        time.sleep(.05)
+                        print(str(cnt) + "번 자리로 가져다 놓음")
+                        ctypes.windll.user32.SetCursorPos(pos[0], pos[1])
+                        ctypes.windll.user32.mouse_event(4, 0, 0, 0, 0)
+                        time.sleep(.05)
+                        pyautogui.click()
+                        cnt += 1
                         raise ValueError
+                    if catchLimit < 0:
+                        print("[Error] 이미지 매칭 부분 무한루프 발생")
+                        im1.save("bug.png")
+                        sys.exit(6794)
                     ax += 1
             except ValueError:
                 break
             ay += 1
         if not catched:
             print("고기를 찾을 수 없음, 스크립트 종료")
+            im1.save("bug.png")
             sys.exit()
 
     print(str(delay) + "초 기다림")
     time.sleep(delay)
 
     print("[Stage 2] 뒤집기")
+    cnt = 1
     for pos in areaWork:
-        print("X_Pos=" + str(pos[0]) + ", Y_Pos=" + str(pos[1]))
-        pyautogui.moveTo(pos[0], pos[1])
-        pyautogui.mouseDown()
-        pyautogui.mouseUp()
-        time.sleep(.15)
+        print(str(cnt) + "번 자리 뒤집음 (" + str(pos[0]) + ", " + str(pos[1]) + ")")
+        ctypes.windll.user32.SetCursorPos(pos[0], pos[1])
+        ctypes.windll.user32.mouse_event(2, 0, 0, 0, 0)
+        ctypes.windll.user32.mouse_event(4, 0, 0, 0, 0)
+        time.sleep(.1)
+        cnt += 1
 
     print(str(delay) + "초 기다림")
     time.sleep(delay)
 
     print("[Stage 3] 제출")
+    cnt = 1
     for pos in areaWork:
-        print("X_Pos=" + str(pos[0]) + ", Y_Pos=" + str(pos[1]))
-        pyautogui.moveTo(pos[0], pos[1])
-        pyautogui.mouseDown()
-        pyautogui.moveTo(dish[0], dish[1])
-        pyautogui.mouseUp()
-        time.sleep(.1)
+        print(str(cnt) + "번 자리 제출 (" + str(pos[0]) + ", " + str(pos[1]) + ")")
+        ctypes.windll.user32.SetCursorPos(pos[0], pos[1])
+        ctypes.windll.user32.mouse_event(2, 0, 0, 0, 0)
+        time.sleep(.05)
+        ctypes.windll.user32.SetCursorPos(dish[0] + random.randint(-5, 5), dish[1] + random.randint(-5, 5))
+        ctypes.windll.user32.mouse_event(4, 0, 0, 0, 0)
+        time.sleep(.05)
+        cnt += 1
 
 input("Ready?")
 
 for pos in startGame:
     print("시작버튼 광클")
-    pyautogui.moveTo(pos[0], pos[1])
-    pyautogui.mouseDown()
-    pyautogui.mouseUp()
+    ctypes.windll.user32.SetCursorPos(pos[0], pos[1])
+    ctypes.windll.user32.mouse_event(2, 0, 0, 0, 0)  # left down
+    time.sleep(.05)
+    ctypes.windll.user32.mouse_event(4, 0, 0, 0, 0)  # left up
+    time.sleep(.05)
 
 while True:
-    mainWork(0, (240, 60, 60), (255, 90, 100), (0, 0)) # 소고기?
-    mainWork(1.5, (240, 50, 50), (255, 170, 170), (-5, 5)) # 삼겹살
+    mainWork(3.2, (240, 60, 60), (255, 90, 100), (0, 0)) # 소고기?
+    mainWork(5, (240, 50, 50), (255, 200, 200), (-5, 5)) # 삼겹살
